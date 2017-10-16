8b94ecfb60687ab74b0fb30fe41ff5ee5c5ca6bc
Leonardo <leonardooc@globo.com>
2017-10-15 23:22:20 -0200

    Pegando comandos
    Pega comandos e o deleta posteriormente.
    Falta corrigir a exibição, implementar um controle de comandos melhor e realmente executar os comandos

--- pppdaemon.py
+++ pppdaemon.py
@@ -7,16 +7,17 @@ host="imap.globo.com"
 log=""
 pwd = ""
 
+
+
 def conectar(log,pwd,host):
     try:
         conn = imaplib.IMAP4_SSL(host)
         conn.login(log,pwd)
         readonly = True
-        conn.select("inbox",readonly)
+        conn.select("inbox")
         return(conn)
     except Exception as e:
-        raise("Nao foi possivel se conectar\n"+str(e))
-
+        print("ERRO NA CONEXAO")
 
 def leremail(conn):
     try:
@@ -26,29 +27,34 @@ def leremail(conn):
         ultimo = listaid[-1]
         resultado,data = conn.fetch(ultimo,"(RFC822)")
         email = data[0][1]
+        #print(conn.list())
+        conn.store(ultimo,'+FLAGS',r'(\Deleted)')
+        conn.expunge()
+        conn.close()
+        conn.logout()
         return(email)
     except IndexError as e:
-        raise ComandoInexistente("Nao ha comandos a serem executados")
+        print("Nao ha comandos a serem executados")
 
 def checkcomando(email):
     try:
-        pattern_comando = re.compile(r'(###comando:)(ponto|print)')
+        pattern_comando = re.compile(r'(###comando:)(ponto|print|getnum)')
         comando = pattern_comando.search(email.decode('utf-8'))
         print(comando.group(2))
         return(comando.group(2))
     except Exception as e:
-        print("Erro")
+        pass
 
 
 if __name__ == "__main__":
 
     while(True):
+        sys.stdout.write("\r{}".format("Rodando..."))
         sys.stdout.flush()
-        sys.stdout.write("{}".format("Rodando..."))
         try:
             conn = conectar(log,pwd,host)
             email = leremail(conn)
             comando = checkcomando(email)
-        except:
-            pass
+        except Exception as e:
+            print(str(e))
         time.sleep(5)
